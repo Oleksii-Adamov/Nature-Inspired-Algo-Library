2642e1ca89eec8a32364a1412522f996a7fed779
Oleksii-Adamov <aleksey.v.adamov@gmail.com>
2021-10-31 02:18:58 +0300

    Implemented Genetic Algo, Unit test for it on TA problem with 4 cities

--- NIA/NIA-test/test.cpp
+++ NIA/NIA-test/test.cpp
@@ -1,6 +1,154 @@
-#include "pch.h" // what is this
+#include "pch.h"
+#include "../NIA/GeneticAlgo.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(GeneticAlgoTest, TA_Test) {
+	static const int CHROMOSOME_SIZE = 4;
+	static const int NUMBER_OF_CITIES = CHROMOSOME_SIZE;
+	static const int NUMBER_OF_INDIVIDUALS = 4;
+	static const int NUMBER_OF_GENERATIONS = 100;
+	static const int NUMBER_OF_ELITES = 1;
+	static const int MUTATION_RATE = 200; // possibility = MUTATION_RATE/32767
+	static const double EPS = 1e-1;
+	struct City { // or Gene
+		int num;
+		double x, y;
+	private:
+		/*static int nint(double x) {
+			return (int)(x + 0.5);
+		}*/
+	public:
+		static double distance(City a, City b) {
+			double delta_x = a.x - b.x;
+			double delta_y = a.y - b.y;
+			return /*nint(*/sqrt(delta_x * delta_x + delta_y * delta_y)/*)*/;
+		}
+	};
+	
+	class Individual : public nia::IndividualInteface<City, CHROMOSOME_SIZE, double> { // or Route
+	public:
+		Individual() {
+			fitness = -1;
+			route_length = -1;
+		}
+		/*Individual(const Individual& t)
+		{
+			fintness = t.fintness;
+			route_lenght = t.route_lenght;
+			for (int i = 0; i < CHROMOSOME_SIZE; i++) {
+				chromosome[i] = t.chromosome[i];
+			}
+		}*/
+		double get_fitness() override {
+			if (abs(fitness - (-1)) < EPS) calc_fitness();
+			return fitness;
+		}
+	private:
+		double fitness;
+		double route_length;
+		static void swap_cities(City& a, City& b) {
+			City t = a;
+			a = b;
+			b = t;
+		}
+		void swap_cities(int i, int j) {
+			swap_cities(chromosome[i], chromosome[j]);
+		}
+		void calc_route_length() {
+			route_length = 0;
+			for (int i = 0; i < NUMBER_OF_CITIES; i++) {
+				if (i + 1 != NUMBER_OF_CITIES) {
+					route_length += City::distance(chromosome[i], chromosome[i + 1]);
+				}
+				else {
+					route_length += City::distance(chromosome[i], chromosome[0]);
+				}
+			}
+		}
+		void calc_fitness() {
+			fitness = 1 / ((double)get_route_length());
+		}
+	public:
+		void mutate() override {
+			for (int i = 0; i < CHROMOSOME_SIZE; i++) {
+				if (rand() < MUTATION_RATE) {
+					int j = rand() % CHROMOSOME_SIZE;
+					swap_cities(i, j);
+				}
+			}
+		}
+		double get_route_length() {
+			if (abs(route_length - (-1)) < EPS) calc_route_length();
+			return route_length;
+		}
+	};
+	auto copy = [](const City from[NUMBER_OF_CITIES], City to[NUMBER_OF_CITIES]) {
+		for (int i = 0; i < NUMBER_OF_CITIES; i++) {
+			to[i] = from[i];
+		}
+	};
+	auto init_population = [&](Individual population[NUMBER_OF_INDIVIDUALS], City cities[NUMBER_OF_CITIES]) {
+		srand(time(0));
+		for (int i = 0; i < NUMBER_OF_INDIVIDUALS; i++) {
+			std::random_shuffle(cities, cities + NUMBER_OF_CITIES);
+			copy(cities, population[i].chromosome);
+		}
+	};
+	std::function<std::pair<Individual, Individual>(const Individual&, const Individual&)> breed = [](const Individual& first_parent, const Individual& second_parent) {
+		auto city_cmp = [](const City& a, const City& b) {
+			return a.num < b.num;
+		};
+		Individual first_child;
+		Individual second_child;
+		int geneA = rand() % CHROMOSOME_SIZE, geneB = rand() % CHROMOSOME_SIZE;
+		int start_gene = std::min(geneA, geneB), end_gene = std::max(geneA, geneB), sub_route_size = end_gene - start_gene + 1;
+		City* a_sub_route = new City[sub_route_size];
+		City* b_sub_route = new City[sub_route_size];
+		for (int i = start_gene; i <= end_gene; i++) {
+			int ind = i - start_gene;
+			a_sub_route[ind] = first_parent.chromosome[i];
+			b_sub_route[ind] = second_parent.chromosome[i];
+			first_child.chromosome[i] = first_parent.chromosome[i];
+			second_child.chromosome[i] = second_parent.chromosome[i];
+		}
+		std::sort(a_sub_route, a_sub_route + sub_route_size, city_cmp);
+		std::sort(b_sub_route, b_sub_route + sub_route_size, city_cmp);
+		int ind_f = 0, ind_s = 0;
+		for (int i = 0; i < CHROMOSOME_SIZE; i++) {
+			if (!std::binary_search(a_sub_route, a_sub_route + sub_route_size, second_parent.chromosome[i], city_cmp)) {
+				if (ind_f == start_gene) ind_f = end_gene + 1;
+				if (ind_f < CHROMOSOME_SIZE) {
+					first_child.chromosome[ind_f] = second_parent.chromosome[i];
+					ind_f++;
+				}
+			}
+			if (!std::binary_search(b_sub_route, b_sub_route + sub_route_size, first_parent.chromosome[i], city_cmp)) {
+				if (ind_s == start_gene) ind_s = end_gene + 1;
+				if (ind_s < CHROMOSOME_SIZE) {
+					second_child.chromosome[ind_s] = first_parent.chromosome[i];
+					ind_s++;
+				}
+			}
+		}
+		delete[] a_sub_route;
+		delete[] b_sub_route;
+		return std::make_pair(first_child, second_child);
+	};
+	City input[NUMBER_OF_CITIES];
+	input[0] = { 1, 0, 0 };
+	input[1] = { 2, 1, 0 };
+	input[2] = { 3, 1, 1 };
+	input[3] = { 4, 0, 1 };
+	Individual* population = new Individual [NUMBER_OF_INDIVIDUALS];
+	init_population(population, input);
+	Individual the_fittest = nia::GeneticAlgo<Individual>::solve(NUMBER_OF_INDIVIDUALS, NUMBER_OF_ELITES, NUMBER_OF_GENERATIONS,
+		population, breed);
+	std::cerr << "[          ] ";
+	for (int i = 0; i < NUMBER_OF_CITIES; i++) {
+		std::cerr << the_fittest.chromosome[i].num << " -> ";
+	}
+	EXPECT_DOUBLE_EQ(the_fittest.get_route_length(), 4);
+	/*EXPECT_TRUE(false) << the_fittest.chromosome[0].num << " -> " << the_fittest.chromosome[1].num << " -> "
+		<< the_fittest.chromosome[2].num << " -> " << the_fittest.chromosome[3].num << " -> " << the_fittest.chromosome[0].num;*/
+
+	delete[] population;
 }
--- NIA/NIA/GeneticAlgo.h
+++ NIA/NIA/GeneticAlgo.h
@@ -1,23 +1,39 @@
 #pragma once
-#include<utility>
+#include <utility>
+#include <functional>
 namespace nia {
 	
 	template<typename Tgene, int chromosome_size, typename Tfitess>
 	class IndividualInteface {
-	protected:
-		Tgene chromosome[chromosome_size];
+		using Individual = IndividualInteface<Tgene, chromosome_size, Tfitess>;
 	public:
-		virtual Tfitess get_fintess();
+		Tgene chromosome[chromosome_size];
+		virtual Tfitess get_fitness() = 0;
+		virtual void mutate() = 0;
+		// copy constructor
+		/*virtual IndividualInteface(const IndividualInteface& t)
+		{
+			for (int i = 0; i < chromosome_size; i++) {
+				chromosome[i] = t.chromosome[i];
+			}
+		}*/
+		/*virtual std::pair<IndividualInteface<Tgene, chromosome_size, Tfitess>, IndividualInteface<Tgene, chromosome_size, Tfitess>> 
+			breed(IndividualInteface<Tgene, chromosome_size, Tfitess>& second_parent); // first parent is this*/
 	};
 	/* genetic algo requires:
-		classes/types:
-			Gene type/class
-			Individual class with public get_fintess method (fintess must have operator < implemented)
-		constants:
+	   constants:
 			chromosome_size
 			NUMBER_OF_INDIVIDUALS (number of individuals in every generation)
 			NUMBER_OF_ELITES (number of best inividuals kept for next generation)
 			NUMBER_OF_GENERATIONS
+
+		classes/types:
+			Gene type/class
+			Individual class with:
+				public get_fintess method (fintess must have operator < implemented)
+				mutation method with build-in mutation_rate - void mutate(Individual &a)
+				static breed method (given two parents, returns two children)- static pair<Individual, Individual> breed(Individual a, Individual b)
+
 		parameters:
 			initial(starting) population - Individual init_population[NUMBER_OF_INDIVIDUALS]
 		functions:
@@ -28,15 +44,83 @@ namespace nia {
 		you may need call this function several times with random initial population, to find more accurate answer
 		performance of algorithm higly relies on performance of get_fitness and comparation of fintess
 	*/
-	template<typename Tgene, int chromosome_size, typename Tfitess>
+	// maybe just typename Individual, but then we don't use inheritance, so I don't know...
+	//template<typename Tgene, int chromosome_size, typename Tfitess>
+	template <typename Individual>
 	class GeneticAlgo {
-		using Individual = IndividualInteface<Tgene, chromosome_size, Tfitess>;
+	private:
+		static bool comp_individ(Individual& l, Individual& r) {
+			return l.get_fitness() > r.get_fitness();
+		}
+		static Individual* mating_pool(const long long NUMBER_OF_INDIVIDUALS, Individual population[], long long size) {
+			Individual* pool = new Individual[size];
+			// Tournament selection
+			for (int i = 0; i < size; i++) {
+				int index1 = rand() % NUMBER_OF_INDIVIDUALS;
+				int index2 = rand() % NUMBER_OF_INDIVIDUALS;
+				if (population[index1].get_fitness() > population[index2].get_fitness()) {
+					pool[i] = population[index1];
+				}
+				else
+					pool[i] = population[index2];
+			}
+			return pool;
+		}
+		static void next_generation(const long long NUMBER_OF_INDIVIDUALS, const long long NUMBER_OF_ELITES, Individual population[], 
+			std::function <std::pair<Individual, Individual>(const Individual&, const Individual&)>& breed) {
+			int m_pool_size = NUMBER_OF_INDIVIDUALS - NUMBER_OF_ELITES;
+			Individual* m_pool = mating_pool(NUMBER_OF_INDIVIDUALS, population, m_pool_size);
+			std::sort(m_pool, m_pool + m_pool_size, comp_individ);
+			for (int i = 0; i < NUMBER_OF_ELITES; i++) {
+				population[i] = m_pool[i];
+				population[i].mutate();
+			}
+			int ind = NUMBER_OF_ELITES;
+			for (int i = 0; ind < NUMBER_OF_INDIVIDUALS; i += 2) {
+				if (i + 1 == m_pool_size) {
+					population[ind] = m_pool[i];
+					break;
+				}
+				std::pair<Individual, Individual> children = breed(m_pool[i], m_pool[i + 1]);
+				population[ind] = children.first;
+				population[ind].mutate();
+				ind++;
+				if (ind < NUMBER_OF_INDIVIDUALS) {
+					population[ind] = children.second;
+					population[ind].mutate();
+					ind++;
+				}
+			}
+			delete[] m_pool;
+		}
 	public:
 		static Individual solve(const long long NUMBER_OF_INDIVIDUALS, const long long NUMBER_OF_ELITES,
-			const long long NUMBER_OF_GENERATIONS, Individual init_population[NUMBER_OF_INDIVIDUALS],
-			void(*mutate)(Individual&),
-			std::pair<Individual, Individual>(*breed)
-			(Individual, Individual));
+			const long long NUMBER_OF_GENERATIONS, Individual init_population[], 
+			std::function <std::pair<Individual, Individual>(const Individual&, const Individual&)>& breed)
+		{
+			Individual* population = new Individual[NUMBER_OF_INDIVIDUALS];
+			Individual prev_best = init_population[0];
+			for (int i = 0; i < NUMBER_OF_INDIVIDUALS; i++) {
+				population[i] = init_population[i];
+				if (prev_best.get_fitness() < population[i].get_fitness()) {
+					prev_best = population[i];
+				}
+			}
+			for (int i = 0; i < NUMBER_OF_GENERATIONS; i++) {
+				next_generation(NUMBER_OF_INDIVIDUALS, NUMBER_OF_ELITES, population, breed);
+				int best = 0;
+				for (int i = 1; i < NUMBER_OF_INDIVIDUALS; i++) {
+					if (population[i].get_fitness() > population[best].get_fitness()) {
+						best = i;
+					}
+				}
+				if (prev_best.get_route_length() == -2 || population[best].get_route_length() < prev_best.get_route_length()) {
+					prev_best = population[best];
+				}
+			}
+			delete[] population;
+			return prev_best;
+		}
 	};
 	/*template<typename Tgene, int chromosome_size, typename Tfitess>
 	IndividualInteface<Tgene, chromosome_size, Tfitess> genetic_algo(const long long NUMBER_OF_INDIVIDUALS, const long long NUMBER_OF_ELITES,
